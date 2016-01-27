4c279b55f0cb81583cd672e401aa4d523e69372c
Aidan Follestad <drummer.aidan@gmail.com>
2016-01-27 13:14:09 -0600

    Added another screen to the sample which will be used to test various functions

--- sample/src/main/java/com/afollestad/appthemeenginesample/misc/MiscActivity.java
+++ sample/src/main/java/com/afollestad/appthemeenginesample/misc/MiscActivity.java
@@ -1,14 +1,25 @@
 package com.afollestad.appthemeenginesample.misc;
 
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.support.annotation.StyleRes;
 
+import com.afollestad.appthemeengine.customizers.ATEActivityThemeCustomizer;
 import com.afollestad.appthemeenginesample.R;
 import com.afollestad.appthemeenginesample.base.BaseThemedActivity;
 
 /**
  * @author Aidan Follestad (afollestad)
  */
-public class MiscActivity extends BaseThemedActivity {
+public class MiscActivity extends BaseThemedActivity implements ATEActivityThemeCustomizer {
+
+    @StyleRes
+    @Override
+    public int getActivityTheme() {
+        // Overrides what's set in the current ATE Config
+        return PreferenceManager.getDefaultSharedPreferences(this).getBoolean("dark_theme", false) ?
+                R.style.AppThemeDark_ActionBar : R.style.AppTheme_ActionBar;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
